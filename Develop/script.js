5d0c2f01046e36d049dde62cd8b07cfc0e7a988c
dutsler1 <dutsler1@gmail.com>
2023-07-20 22:11:22 -0600

    added the rest of the javascript code to enable the password to auto generate

--- Develop/script.js
+++ Develop/script.js
@@ -1,65 +1,75 @@
-// Assignment Code
+
+
+
 var generateBtn = document.querySelector("#generate");
-
+//this is generating the basics of the password
 function generatePassword() {
-//VARIABLES FOR THE PASSWORD
-  var lowercase =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-  var uppercase =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-  var numeric =["1","2","3","4","5","6","7","8","9"]
-  var special =["!","@","#","$","%","^","&","*","(",")"]
-
-  // THEN I am presented with a series of prompts for password criteria
-  // WHEN prompted for password criteria
-  // THEN I select which criteria to include in the password
-  // WHEN prompted for the length of the password
-  var passwordLength = window.prompt("How long should the password be? must be between 8 and 128")
-  // THEN I choose a length of at least 8 characters and no more than 128 characters
-  if (passwordLength < 8) {
-    window.alert("password must be between 8 and 128 characters long!")
-    return
+  var alphabet = "abcdefghijklmnopqrstuvwxyz";
+  var numeric = "0123456789";
+  var special = "!@#$%^&*()";
+  var lowercase = alphabet.split("")
+  var uppercase = alphabet.toUpperCase().split("")
+  var verify = {
+    confirmPasswordLowercase: false,
+    confirmPasswordNumeric: false,
+    confirmPasswordSpecial: false,
+    confirmPasswordUppercase: false,
+  };
+//this is a window prompt that tells the user what the criteria is and if it is wrong what they need
+  var passwordLength = window.prompt("How long should the password be? must be between 8 and 128");
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Password must be between 8 and 128 characters long!");
+    return "";
   }
-  else if (passwordLength > 128) {
-    window.alert("password must be between 8 and 128 characters long!")
-    return
+// this is asking the user what criteria they want 
+  var confirmPasswordLowercase = window.confirm("Do you want to have lowercase letters?");
+  var confirmPasswordUppercase = window.confirm("Do you want to have uppercase letters?");
+  var confirmPasswordNumeric = window.confirm("Do you want to include numeric characters?");
+  var confirmPasswordSpecial = window.confirm("Do you want to include special characters?");
+  verify = {
+    confirmPasswordLowercase,
+    confirmPasswordNumeric,
+    confirmPasswordSpecial,
+    confirmPasswordUppercase,
+  };
+
+  var character = "";
+  if (confirmPasswordLowercase) {
+    character += lowercase;
+  }
+  if (confirmPasswordUppercase) {
+    character += uppercase;
+  }
+  if (confirmPasswordNumeric) {
+    character += numeric;
+  }
+  if (confirmPasswordSpecial) {
+    character += special;
+  }
+// this is saying if there is nothing inside the box that they need at least something
+  if (character === "") {
+    window.alert("Please choose at least one character type!");
+    return "";
   }
 
-  // WHEN asked for character types to include in the password
-  // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-  var confirmPasswordLowercase = window.confirm("do you want to have lowercase letters?")
-  var confirmPasswordUppercase = window.confirm("do you want to have uppercase letters?")
-  var confirmPasswordNumeric = window.confirm("do you want to include numeric characters?")
-  var confirmPasswordSpecial = window.confirm("do you want to include special characters?")
-
-  // WHEN I answer each prompt
-  // THEN my input should be validated and at least one character type should be selected
-  if (confirmPasswordLowercase === false && confirmPasswordUppercase === false && confirmPasswordNumeric === false && confirmPasswordSpecial === false) {
-    window.alert("please choose one option!")
-    return
+  var password = "";
+  for (var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * character.length);
+    password += character.charAt(randomIndex);
   }
 
-
-  // WHEN all prompts are answered
-  // THEN a password is generated that matches the selected criteria
-  // WHEN the password is generated
-  // THEN the password is either displayed in an alert or written to the page
-  // ```
-
+  return password;
 }
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  if (password) {
+    passwordText.value = password;
+    window.alert("Congratulations! The password has been generated successfully.");
+  } else {
+    passwordText.value = "";
+  }
 }
 
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
